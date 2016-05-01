c2421e9901d686885a87320ef264ab9946601ccf
mc10051091 <mc10051091@ufg.edu.sv>
2016-05-01 10:33:31 -0600

    fea
    fea

--- AjaxCurdjTableServlet/src/com/dao/CrudDao.java
+++ AjaxCurdjTableServlet/src/com/dao/CrudDao.java
@@ -4,7 +4,7 @@ package com.dao;
 //tercera locura remota
 //cuarto locura remota
 //quinta locura remota
-//buena
+//fea
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
